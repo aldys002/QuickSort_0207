bc217c765d61d42cf568623048ce56467cf6a45c
aldys002 <igidiaaldys@gmail.com>
2024-03-28 14:23:41 +0700

    membuat prosedur display

--- quickshort.cpp
+++ quickshort.cpp
@@ -73,12 +73,22 @@ void q_short( int low, int high)
         //swap the element at index i whit the element at index j
         swap (i, j);
     }
-}
+
 
 if (low < j ) { //step 11
     //swap the pivot element with the element at index j 
     swap (low, j);
 }
 
-//recursive call to sort the left sub array
-q_short(low, j - 1); //step 12
+    //recursive call to sort the left sub array
+    q_short(low, j - 1); //step 12
+
+    //recursive call to sort the right sub array
+
+    q_short(j + 1, high); //step 13
+
+}
+
+void display(){
+    
+}
