c08a7dd1deb441a334387f849181d86254c8cf97
aldys002 <igidiaaldys@gmail.com>
2024-03-28 14:15:56 +0700

    membuat fungsi if

--- quickshort.cpp
+++ quickshort.cpp
@@ -69,4 +69,12 @@ void q_short( int low, int high)
     }
     cmp_count++;
     if (i < j) // step 9
+    {
+        //swap the element at index i whit the element at index j
+        swap (i, j);
+    }
+}
+
+if (low < j ) {
+    
 }
