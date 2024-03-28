65ead36ca3a63306bce669660c64e816fac23b2b
aldys002 <igidiaaldys@gmail.com>
2024-03-28 14:09:58 +0700

    menambah step 5

--- quickshort.cpp
+++ quickshort.cpp
@@ -52,4 +52,8 @@ void q_short( int low, int high)
     j = high;    // step 4
 
     while (i <= j) //step 10
+    {
+        //search fpr an element greater than pivot
+        while ((arr[i] <= pivot ) && (i <= high)) //step 5
+    }
 }
